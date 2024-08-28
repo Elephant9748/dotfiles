9e252e55170bbc1f18b2975e1f7797239053ad2a
@Elephant9748 <adytamasurya@protonmail.com>
2024-08-28 13:59:20 +0700

    add: vda

--- .config/arch/fish/config.fish
+++ .config/arch/fish/config.fish
@@ -62,7 +62,7 @@ if command -v git > /dev/null
         abbr -a ga 'git add -p'
         abbr -a gl 'git log --all --decorate --oneline --graph'
         abbr -a gs 'git status'
-        abbr -a gsf 'git diff --cached'
+        abbr -a gss 'git diff --cached'
 end
 
 #A cat clone with syntax highlighting https://github.com/sharkdp/bat
@@ -129,7 +129,7 @@ if status --is-interactive
    #set PIP_VENV "$HOME/.venv/bin/"
    #source "$PIP_VENV/activate.fish"
 
-   #base16_shell
+   #base16_shell -->> git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
    set BASE16_SHELL "$HOME/.config/base16-shell/"
    source "$BASE16_SHELL/profile_helper.fish"
 
@@ -356,7 +356,7 @@ function fish_greeting
 	echo -e " \\e[1mDisk usage:\\e[0m"
 	echo
 	echo -ne (\
-		df -l -h | grep -E 'dev/(xvda|sd|mapper)' | \
+		df -l -h | grep -E 'dev/(xvda|sd|mapper|vda)' | \
 		awk '{printf "\\\\t%s\\\\t%4s / %4s  %s\\\\n\n", $6, $3, $2, $5}' | \
 		sed -e 's/^\(.*\([8][5-9]\|[9][0-9]\)%.*\)$/\\\\e[0;31m\1\\\\e[0m/' -e 's/^\(.*\([7][5-9]\|[8][0-4]\)%.*\)$/\\\\e[0;33m\1\\\\e[0m/' | \
 		paste -sd ''\
