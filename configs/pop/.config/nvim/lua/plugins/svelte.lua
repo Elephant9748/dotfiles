00355752a89e33dd62d36315d7f676c1db18c941
@Elephant9748 <adytamasurya@protonmail.com>
2025-12-13 09:47:35 +0700

    del: config pop

--- configs/pop/.config/nvim/lua/plugins/svelte.lua
+++
@@ -1,6 +0,0 @@
-return {
-        -- svelte
-        { 'othree/html5.vim' },
-        { 'pangloss/vim-javascript'},
-        { 'evanleck/vim-svelte' },
-}
--- configs/pop/.config/nvim/lua/servers/luau.lua
+++
@@ -1,7 +0,0 @@
-return function(lspconfig, capabilities, on_attach)
-        -- luau
-        lspconfig.luau_lsp.setup{
-                capabilities = capabilities,
-                on_attach = on_attach,
-        }
-end
