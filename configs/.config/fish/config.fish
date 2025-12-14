8f1d2955917bd73f6dc72a81fba5b647b2bcc07c
@Elephant9748 <adytamasurya@protonmail.com>
2025-12-14 21:16:26 +0700

    fish: another check command, remove abbr for nix

--- configs/.config/fish/config.fish
+++ configs/.config/fish/config.fish
@@ -25,47 +25,51 @@ function fish_greeting
 	echo -e " \\e[1mNetwork:\\e[0m"
 	echo
 	# http://tdt.rocks/linux_network_interface_naming.html
-	echo -ne (\
-		ip addr show up scope global | \
-			grep -E ': <|inet' | \
-			sed \
-				-e 's/^[[:digit:]]\+: //' \
-				-e 's/: <.*//' \
-				-e 's/.*inet[[:digit:]]* //' \
-				-e 's/\/.*//'| \
-			awk 'BEGIN {i=""} /\.|:/ {print i" "$0"\\\n"; next} // {i = $0}' | \
-			sort | \
-			column -t -R1 | \
-			# public addresses are underlined for visibility \
-			sed 's/ \([^ ]\+\)$/ \\\e[4m\1/' | \
-			# private addresses are not \
-			sed 's/m\(\(10\.\|172\.\(1[6-9]\|2[0-9]\|3[01]\)\|192\.168\.\).*\)/m\\\e[24m\1/' | \
-			# unknown interfaces are cyan \
-			sed 's/^\( *[^ ]\+\)/\\\e[36m\1/' | \
-			# ethernet interfaces are normal \
-			sed 's/\(\(en\|enp\|em\|eth\)[^ ]* .*\)/\\\e[39m\1/' | \
-			# wireless interfaces are purple \
-			sed 's/\(wl[^ ]* .*\)/\\\e[35m\1/' | \
-			# wwan interfaces are yellow \
-			sed 's/\(ww[^ ]* .*\).*/\\\e[33m\1/' | \
-			sed 's/$/\\\e[0m/' | \
-			sed 's/^/\t/' \
-		)
-      echo
-      set_color 16AA64
-      echo -e ' note!: '
-      set_color 6A6362
-      echo -e '\t*created emergency backup (tracy,proton)'
-      set_color F53C3C
-      echo -e '\t*supply chain attack: check dependencies npm packages.!!! '
-      set_color 605F5E
-      echo -e '\t*https://socket.dev/blog/npm-author-qix-compromised-in-major-supply-chain-attack'
-      echo -e '\t*https://gist.github.com/sindresorhus/2b7466b1ec36376b8742dc711c24db20'
-      echo -e '\t*https://github.com/chalk/chalk/issues/656'
-      set_color F53C3C
-      echo -e '\t*DONT UPRADE webtorrent-cli, prettier yet'
-      set_color normal
-      echo 
+    if command -q ip
+            echo -ne (\
+                    ip addr show up scope global | \
+                    grep -E ': <|inet' | \
+                    sed \
+                    -e 's/^[[:digit:]]\+: //' \
+                    -e 's/: <.*//' \
+                    -e 's/.*inet[[:digit:]]* //' \
+                    -e 's/\/.*//'| \
+                    awk 'BEGIN {i=""} /\.|:/ {print i" "$0"\\\n"; next} // {i = $0}' | \
+                    sort | \
+                    column -t -R1 | \
+                    # public addresses are underlined for visibility \
+                    sed 's/ \([^ ]\+\)$/ \\\e[4m\1/' | \
+                    # private addresses are not \
+                    sed 's/m\(\(10\.\|172\.\(1[6-9]\|2[0-9]\|3[01]\)\|192\.168\.\).*\)/m\\\e[24m\1/' | \
+                    # unknown interfaces are cyan \
+                    sed 's/^\( *[^ ]\+\)/\\\e[36m\1/' | \
+                    # ethernet interfaces are normal \
+                    sed 's/\(\(en\|enp\|em\|eth\)[^ ]* .*\)/\\\e[39m\1/' | \
+                    # wireless interfaces are purple \
+                    sed 's/\(wl[^ ]* .*\)/\\\e[35m\1/' | \
+                    # wwan interfaces are yellow \
+                    sed 's/\(ww[^ ]* .*\).*/\\\e[33m\1/' | \
+                    sed 's/$/\\\e[0m/' | \
+                    sed 's/^/\t/' \
+                    )
+            echo
+    end
+    set_color 16AA64
+    echo -e ' note!: '
+    if string match -q "rigel" (whoami)
+            set_color 6A6362
+            echo -e '\t*created emergency backup (tracy,proton)'
+            set_color F53C3C
+            echo -e '\t*supply chain attack: check dependencies npm packages.!!! '
+            set_color 605F5E
+            echo -e '\t*https://socket.dev/blog/npm-author-qix-compromised-in-major-supply-chain-attack'
+            echo -e '\t*https://gist.github.com/sindresorhus/2b7466b1ec36376b8742dc711c24db20'
+            echo -e '\t*https://github.com/chalk/chalk/issues/656'
+            set_color F53C3C
+            echo -e '\t*DONT UPRADE webtorrent-cli, prettier yet'
+            set_color normal
+    end
+    echo 
 
 end
 
@@ -310,13 +314,10 @@ if command -v eza > /dev/null
 	abbr -a lg 'eza -l -G'
 	abbr -a llg 'eza -la -G'
 	abbr -a lll 'eza -la'
-    abbr -a nix 'nix-env'
 else
 	abbr -a l 'ls'
 	abbr -a ll 'ls -l'
 	abbr -a lll 'ls -la'
-    abbr -a vi 'vi'
-    abbr -a vim 'vim'
 end
 
 # df dont show tmpfs
