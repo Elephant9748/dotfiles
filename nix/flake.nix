9eb60bd07b9331001c8cc1c4d8da1291d352d0cd
@Elephant9748 <adytamasurya@proton.me>
2026-02-01 22:04:03 +0700

    nix: binary cache for Nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -18,6 +18,13 @@
 {
   description = "Nixos Flake";
 
+  nixConfig = {
+    extra-substituters = [ "https://cache.numtide.com" ];
+    extra-trusted-public-keys = [
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+    ];
+  };
+
   inputs = {
 	nixpkgs-unstable = {
         url = "github:nixos/nixpkgs/nixos-unstable";	
