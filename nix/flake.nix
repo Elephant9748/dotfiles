17fe8be818ffdeb7476efd4084e9303b0fccf2e1
@Elephant9748 <adytamasurya@proton.me>
2026-02-27 05:02:57 +0700

    nix: default to unstable pkgs

--- nix/flake.nix
+++ nix/flake.nix
@@ -12,22 +12,22 @@
 
     # STABLE
     # --------------------------------------------------------------
-	nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";	
-
-	home-manager = {                                                  
-        url = "github:nix-community/home-manager/release-25.11";
-        inputs.nixpkgs.follows = "nixpkgs";
-    };
+	# nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";	
+	#
+	# home-manager = {                                                  
+	#        url = "github:nix-community/home-manager/release-25.11";
+	#        inputs.nixpkgs.follows = "nixpkgs";
+	#    };
     # --------------------------------------------------------------
 
     # UNSTABLE
     # --------------------------------------------------------------
-	# nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";	
-	#
-	# home-manager = {                                                  
-	#        url = "github:nix-community/home-manager";
-	#        inputs.nixpkgs.follows = "nixpkgs";
-	#    };
+	nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";	
+
+	home-manager = {                                                  
+	       url = "github:nix-community/home-manager";
+	       inputs.nixpkgs.follows = "nixpkgs";
+	   };
     # --------------------------------------------------------------
 
     
