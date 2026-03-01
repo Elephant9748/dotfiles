656638122a2d7c442c63873b197b913fd32955b0
@Elephant9748 <adytamasurya@proton.me>
2026-03-01 19:39:46 +0700

    nix: Nix-ld

--- nix/modules/nix_ld.nix
+++ nix/modules/nix_ld.nix
@@ -1,18 +1,21 @@
+# find package that missing lib:
+# nix run github:nix-community/nix-index-database -- lib/libxkbcommon.so.0 --top-level
+
 { pkgs, ... }: {
         programs.nix-ld.enable = true;
         programs.nix-ld.libraries = with pkgs; [
+                # stdenv.cc.cc
+                # glibc
+                # libxcb
+                # libX11
+                # libXrandr
+                # libXcursor
+                # libXi
+                # libGL
+                # alsa-lib
+                # udev
+                # libxkbcommon
                 # libstdc++
-                stdenv.cc.cc
-                glibc
                 # libgcc_s
-                libxcb
-                libX11
-                libXrandr
-                libXcursor
-                libXi
-                libGL
-                alsa-lib
-                udev
-                libxkbcommon
         ];
 }
