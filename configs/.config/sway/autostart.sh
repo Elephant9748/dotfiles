a21a3bd8b8ded75494e673d8e068ce6cc9a09bfd
@Elephant9748 <adytamasurya@proton.me>
2026-05-31 22:33:28 +0700

    sway: rename swaypaper to swayper

--- configs/.config/sway/autostart.sh
+++ configs/.config/sway/autostart.sh
@@ -5,4 +5,4 @@ autotiling-rs -w 6 2 &
 wl-paste --type text --watch cliphist store &
 wl-paste --type image --watch cliphist store &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-swaypaper -d ~/Pictures/wallhaven.cc/ &
+swayper -d ~/Pictures/wallhaven.cc/ &
