d941374548b9a5e5b1cd1abce5ce51e139ffa855
@Elephant9748 <adytamasurya@proton.me>
2026-06-03 19:58:11 +0700

    mango: autostart correct bin

--- configs/.config/mango/autostart-nix.sh
+++ configs/.config/mango/autostart-nix.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/run/current-system/sw/bin/sh
 
 # waybar
-pkill -f waybar
-waybar -c ~/.config/waybar/mango-config -s ~/.config/waybar/mango-style.css &
+/run/current-system/sw/bin/pkill -f waybar
+/etc/profiles/per-user/ringo/bin/waybar -c ~/.config/waybar/mango-config -s ~/.config/waybar/mango-style.css &
 
 # wallpaper
-swaybg -i ~/Pictures/wallhaven.cc/vii.png &
+/etc/profiles/per-user/ringo/bin/swaybg -i ~/Pictures/wallhaven.cc/vii.png &
 
 # extra
 # /usr/lib/xdg-desktop-portal-wlr &
-wl-paste --type text --watch cliphist store &
-wl-paste --type image --watch cliphist store &
+/etc/profiles/per-user/ringo/bin/wl-paste --type text --watch cliphist store &
+/etc/profiles/per-user/ringo/bin/wl-paste --type image --watch cliphist store &
 # /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
