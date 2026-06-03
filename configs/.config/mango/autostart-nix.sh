144a5d07dc5c1da021fb042534b57cfb37a91237
@Elephant9748 <adytamasurya@proton.me>
2026-06-04 00:43:49 +0700

    mango: autostart correct bin path for nix

--- configs/.config/mango/autostart-nix.sh
+++ configs/.config/mango/autostart-nix.sh
@@ -1,14 +1,16 @@
 #!/run/current-system/sw/bin/sh
 
+user=$(whoami)
+
 # waybar
 /run/current-system/sw/bin/pkill -f waybar
-/etc/profiles/per-user/ringo/bin/waybar -c ~/.config/waybar/mango-config -s ~/.config/waybar/mango-style.css &
+/etc/profiles/per-user/${user}/bin/waybar -c ~/.config/waybar/mango-config -s ~/.config/waybar/mango-style.css &
 
 # wallpaper
-/etc/profiles/per-user/ringo/bin/swaybg -i ~/Pictures/wallhaven.cc/vii.png &
+/etc/profiles/per-user/${user}/bin/swaybg -i ~/Pictures/wallhaven.cc/vii.png &
 
 # extra
 # /usr/lib/xdg-desktop-portal-wlr &
-/etc/profiles/per-user/ringo/bin/wl-paste --type text --watch cliphist store &
-/etc/profiles/per-user/ringo/bin/wl-paste --type image --watch cliphist store &
+/etc/profiles/per-user/${user}/bin/wl-paste --type text --watch cliphist store &
+/etc/profiles/per-user/${user}/bin/wl-paste --type image --watch cliphist store &
 # /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
