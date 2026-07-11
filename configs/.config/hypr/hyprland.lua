602abadf4b6aa36b093d121f2a2f25c0bab29ab3
@Elephant9748 <adytamasurya@proton.me>
2026-07-12 02:43:56 +0700

    hypr: vrr on event if error

--- configs/.config/hypr/hyprland.lua
+++ configs/.config/hypr/hyprland.lua
@@ -25,7 +25,8 @@ hl.monitor({
     output = "HDMI-A-1",
     mode = "1920x1080@60",
     position = "0x0",
-    scale = "1",
+    scale = 1,
+    -- vrr = 0
 })
 
 hl.on("hyprland.start", function()
