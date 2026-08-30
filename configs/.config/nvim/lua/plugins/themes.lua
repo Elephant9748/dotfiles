5877f3ea8d9ec4b83d62e996e83cb4b481685847
@Elephant9748 <adytamasurya@proton.me>
2026-08-31 03:26:30 +0700

    nvim: colorscheme

--- configs/.config/nvim/lua/plugins/themes.lua
+++ configs/.config/nvim/lua/plugins/themes.lua
@@ -16,14 +16,10 @@ return {
                         -- ]]
                 end,
         },
+        'jakiurcore/fleury.nvim',
+        'srcery-colors/srcery-vim',
         'tinted-theming/base16-vim',
-        'stevedylandev/darkmatter-nvim',
         'nanotech/jellybeans.vim',
         -- 'ryross/ryderbeans',
         'gruvbox-community/gruvbox',
-        'vim-scripts/candyman.vim',
-        'rafi/awesome-vim-colorschemes',
-        {
-                "catppuccin/nvim", name = "catppuccin", priority = 1000 
-        },
 }
